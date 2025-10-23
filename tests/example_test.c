9b5c12572fa64510e5d67de0fade48fb8370433d
theodegeest <theo.degeest@gmail.com>
2025-10-23 21:20:05 +0200

    tests

--- tests/example_test.c
+++ tests/example_test.c
@@ -11,3 +11,20 @@ Test(message, allocation) {
   cr_expect(*(int *)message->payload == 42, "Message should carry a payload.");
   message_free(message);
 }
+
+// Test(server_client, test1) {
+
+//   ActorUniverse *actor_universe = actor_universe_make();
+//   Threadpool *threadpool = threadpool_make(actor_universe, 8);
+
+
+
+//   threadpool_stop(threadpool);
+//   threadpool_free(threadpool);
+//   actor_universe_free(actor_universe);
+
+//   int x = 42;
+//   Message *message = message_make(&x);
+//   cr_expect(*(int *)message->payload == 42, "Message should carry a payload.");
+//   message_free(message);
+// }
